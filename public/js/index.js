8dc0420568e69bb07221a448bdc8c4dbbe6668c1
William Blake <willblakebooking@gmail.com>
2018-09-15 13:55:57 -0500

    ...

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,7 @@
 var todos = ["clean room", "make bed", "wash car"];
 
 function submitTodo() {
-  var getElement = document.getElementById("todos");
   for (var i = 0; i < todos.length; i++) {
-    getElement.innerHTML += '<li>' + todos[i] + '</li>';
+    document.getElementById("todos").innerHTML += '<li>' + todos[i] + '</li>';
   };
 };
