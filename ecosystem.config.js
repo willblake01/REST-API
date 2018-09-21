5d0fca834d5c6e9040efaea94226b8a480074524
William Blake <willblakebooking@gmail.com>
2018-09-21 08:14:52 -0500

    ...

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  apps : [{
-    name      : 'REST-API',
-    script    : 'server.js',
+  apps: [{
+    name: 'REST-API',
+    script: 'server.js',
     env: {
       NODE_ENV: 'development'
     },
-    env_production : {
+    env_production: {
       NODE_ENV: 'production'
     }
   }],
 
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:willblake01/REST-API.git',
+  deploy: {
+    production: {
+      user: 'node',
+      host: '212.83.163.1',
+      ref: 'origin/master',
+      repo: 'git@github.com:willblake01/REST-API.git',
       path: '/var/www/rest-cms.herokuapp.com',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
 };
